b230f7f9e262b2d291da493a69e7ae8b21d7cbf8
Dusan <jovic.dusan5@gmail.com>
2025-11-05 21:53:33 +0100

    Implementirana opcija za dodavanje knjiga

--- library/app/admin/admin.js
+++ library/app/admin/admin.js
@@ -51,4 +51,32 @@ tabelaTijelo.addEventListener('click', (e) => {
     }
 });
 
+const forma = document.getElementById('dodaj-knjigu-form');
+
+if (forma) {
+    forma.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const noviId = knjige.length > 0 ? knjige[knjige.length - 1].identifikator + 1 : 1;
+
+        const novaKnjiga = new Book(
+            noviId,
+            document.getElementById('naziv').value,
+            document.getElementById('datum').value,
+            document.getElementById('url').value,
+            document.getElementById('opis').value,
+            parseInt(document.getElementById('popularnost').value)
+        );
+
+        knjige.push(novaKnjiga);
+
+        localStorage.setItem('knjige', JSON.stringify(knjige));
+
+        prikaziKnjige();
+        forma.reset();
+
+        alert(`Nova knjiga "${novaKnjiga.nazivKnjige}" je uspešno dodata!`);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', prikaziKnjige);
